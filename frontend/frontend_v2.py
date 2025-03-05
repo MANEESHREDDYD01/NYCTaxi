e7cb53be77600905e1bb5e20eafefd517f116be5
MANEESHREDDYD01 <132993619+MANEESHREDDYD01@users.noreply.github.com>
2025-03-05 03:51:17 -0500

    Add files via upload

--- frontend/frontend_v2.py
+++ frontend/frontend_v2.py
@@ -1,9 +1,10 @@
 import sys
 from pathlib import Path
 
-parent_dir = str(Path(__file__).parent.parent)
+parent_dir = str(Path(_file_).parent.parent)
 sys.path.append(parent_dir)
 
+import pytz
 
 import zipfile
 
@@ -215,9 +216,18 @@ def load_shape_data_file(
 
 # st.set_page_config(layout="wide")
 
-current_date = pd.Timestamp.now(tz="Etc/UTC")
+
+
+#current_date = pd.Timestamp.now(tz="Etc/UTC")
+#st.title(f"New York Yellow Taxi Cab Demand Next Hour")
+#st.header(f'{current_date.strftime("%Y-%m-%d %H:%M:%S")}')
+
+# Convert UTC to New York Time (EST/EDT)
+nyc_tz = pytz.timezone("America/New_York")
+current_date = pd.Timestamp.now(tz="UTC").tz_convert(nyc_tz)
+
 st.title(f"New York Yellow Taxi Cab Demand Next Hour")
-st.header(f'{current_date.strftime("%Y-%m-%d %H:%M:%S")}')
+st.header(f'{current_date.strftime("%Y-%m-%d %H:%M:%S %Z")}')  # Added timezone
 
 progress_bar = st.sidebar.header("Working Progress")
 progress_bar = st.sidebar.progress(0)
